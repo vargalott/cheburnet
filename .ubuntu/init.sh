499bb04cb7fb3f23c66a1fc33039b0f396a96ca6
Mykola Symon <vargalott@gmail.com>
2026-04-04 20:33:53 +0300

    config(.ubuntu/init.sh)

--- .ubuntu/init.sh
+++ .ubuntu/init.sh
@@ -10,7 +10,7 @@ init_system() {
     apt-get -y install bc bmon btop certbot cron curl dnsutils htop iftop jq micro nano net-tools util-linux uuid-runtime vnstat wget
 
     # docker
-    curl -fsSL https://get.docker.com | sh
+    wget -qO- https://get.docker.com | sh
     docker network create --driver bridge --subnet=172.20.0.0/24 --gateway=172.20.0.1 localnet
 
     # disable unattended upgrades
@@ -22,30 +22,26 @@ EOF
 }
 
 configure_sysctl() {
-    wget -qO /etc/sysctl.conf https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/resolved.conf \
-        || { echo "sysctl.conf unavailable"; exit 1; }
+    wget -qO /etc/sysctl.conf https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/sysctl.conf
 
     sysctl --system
 }
 
 configure_ssh() {
     local ssh_key="$1"
+    [[ -z "$ssh_key" ]] && return
 
-    if [[ -n "$ssh_key" ]]; then
-        wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/resolved.conf \
-            || { echo "sshd_config unavailable"; exit 1; }
+    wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/sshd_config
 
-        mkdir -p ~/.ssh && chmod 700 ~/.ssh
-        touch ~/.ssh/authorized_keys && echo "$ssh_key" > ~/.ssh/authorized_keys
-        chmod 600 ~/.ssh/authorized_keys
+    mkdir -p ~/.ssh && chmod 700 ~/.ssh
+    touch ~/.ssh/authorized_keys && echo "$ssh_key" > ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
 
-        systemctl daemon-reload && systemctl enable --now ssh.socket
-    fi
+    systemctl daemon-reload && systemctl enable --now ssh.socket
 }
 
 configure_dns() {
-    wget -qO /etc/systemd/resolved.conf https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/resolved.conf \
-        || { echo "resolved.conf unavailable"; exit 1; }
+    wget -qO /etc/systemd/resolved.conf https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/resolved.conf
 
     ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
     systemctl daemon-reload && systemctl enable --now systemd-resolved
@@ -58,14 +54,13 @@ configure_cron() {
 configure_ssl() {
     local cert_email="$1"
     local cert_domain="$2"
+    [[ -z "$cert_email" || -z "$cert_domain" ]] && return
 
-    [[ -n "$cert_email" ]] && [[ -n "$cert_domain" ]] \
-        && certbot certonly --standalone --agree-tos -m "$cert_email" -d "$cert_domain" --non-interactive
+    certbot certonly --standalone --agree-tos -m "$cert_email" -d "$cert_domain" --non-interactive
 }
 
 configure_shell() {
-    wget -qO ~/.bashrc https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/.bashrc \
-        || { echo ".bashrc unavailable"; exit 1; }
+    wget -qO ~/.bashrc https://raw.githubusercontent.com/vargalott/cheburnet/main/.ubuntu/.bashrc
 }
 
 # all root
@@ -73,10 +68,6 @@ main() {
     local ssh_key="${IU_SSH_KEY:-}"
     local cert_email="${IU_CERT_EMAIL:-}"
     local cert_domain="${IU_CERT_DOMAIN:-}"
-
-    echo "SSH key: $ssh_key"
-    echo "Email for domain certification: $cert_email"
-    echo "Certified domain: $cert_domain"
 
     sleep 1 && init_system
     sleep 1 && configure_sysctl
